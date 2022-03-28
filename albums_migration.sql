e72327f0059ff78eb8cbb09d179b243d8447b839
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-28 09:53:01 -0500

    Completed Exercise for Table Creation

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,14 @@
 drop table if exists albulms;
 create table if not exists albulms(
-                                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                      artist varchar(50),
-    albulm_name varchar(100) ,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist varchar(255),
+    albulm_name varchar(255) ,
     release_date int,
-    sales float ,
-    genres varchar(150),
+    sales decimal(12,6) ,
+    genres varchar(255),
     primary key(id)
 );
+
+show table;
+
+describe albums;
