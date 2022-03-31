bc351d57016e2016ade678be886d316d0187579e
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-31 13:44:03 -0500

    completed indexes_exercise.sql

---
+++ indexes_exercise.sql
@@ -0,0 +1,7 @@
+# CREATE INDEX [index name]
+#     ON [Table name]([column1, column2, column3,...]);
+USE raymond;
+
+CREATE INDEX albums_name_combo ON albums (album_name, artist);
+
+DESCRIBE albums;
---
+++ subqueries_exercises.sql
@@ -0,0 +1,51 @@
+#TODO:
+# Find all the employees with the same hire date as employee 101010 using a subquery.69 Rows
+SELECT CONCAT(first_name, ' ', last_name) AS EMPLOYEE
+FROM employees
+WHERE hire_date = (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+#TODO:
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+SELECT COUNT(title)
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+#TODO:
+# Find all the current department managers that are female.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date >= CURDATE()
+)
+  AND gender = 'F';
+
+#TODO:
+# BONUS:Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM dept_manager
+        WHERE to_date >= CURDATE()
+    )
+      AND emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+    )
+);
+
